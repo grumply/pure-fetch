43261a5fe9c565099a135baa76d2b0a25b11efb0
Sean Hickman <sean@grumply.com>
2020-02-13 23:31:51 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-core, pure-cache, pure-async, pure-try, pure-default, stdenv }:
 mkDerivation {
   pname = "pure-async";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-cache pure-core pure-async pure-try pure-default ];
   homepage = "github.com/grumply/pure-async";
